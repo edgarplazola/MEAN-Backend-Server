e429475fa86eaccb136d22c19c0678b705b99044
edgarplazola <edgarplazola@gmail.com>
2019-08-24 23:42:42 -0500

    Corrigiendo error de token requerido

--- routes/login.js
+++ routes/login.js
@@ -19,15 +19,10 @@ const client = new OAuth2Client(CLIENT_ID);
 async function verify(token) {
     const ticket = await client.verifyIdToken({
         idToken: token,
-        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        audience: CLIENT_ID
     });
 
     const payload = ticket.getPayload();
-    // const userid = payload['sub'];
-    // If request specified a G Suite domain:
-    //const domain = payload['hd'];
     return {
         nombre: payload.name,
         email: payload.email,
@@ -35,7 +30,6 @@ async function verify(token) {
         nombre: true
     }
 }
-verify().catch(console.error);
 
 app.post('/google', async (req, res) => {
 
